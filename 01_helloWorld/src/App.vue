6ac90c2ce29561070339452d6bcd5ec63d7f9daf
zhanglixin17 <91523639+leslieXin92@users.noreply.github.com>
2022-04-13 21:38:56 +0800

    helloWorld

---
+++ 01_helloWorld/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <!-- 可以有多个根标签 -->
+    <img src="./assets/logo.png" />
+    <HelloWorld msg="hello yahoo" />
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+    name: 'App',
+    components: {
+        HelloWorld,
+    },
+}
+</script>
+
+<style scope>
+.img {
+    width: 100px;
+    height: 100px;
+}
+</style>
