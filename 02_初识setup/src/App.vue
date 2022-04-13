86a29399ca4b3b89877e3a0500f5cf6670168d8f
zhanglixin17 <91523639+leslieXin92@users.noreply.github.com>
2022-04-13 22:26:12 +0800

    初识setup

---
+++ 02_初识setup/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <h1> hello yahoo </h1>
+    <h2>name：{{ name }}</h2>
+    <h2>age：{{ age }}</h2>
+    <button @click="showMsg"> showMsg </button>
+</template>
+
+<script>
+export default {
+    name: 'App',
+    setup () {
+        // 数据
+        const name = 'yahoo'
+        let age = 23
+        // 方法
+        function showMsg () {
+            alert(`${name},${age}`)
+        }
+        // 返回
+        return { name, age, showMsg }
+    }
+}
+</script>
