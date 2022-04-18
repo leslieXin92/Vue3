13a6a4d9fc0ec834a937f774fdf7bcd368590c3f
zhanglixin17 <91523639+leslieXin92@users.noreply.github.com>
2022-04-19 00:26:04 +0800

    provide and inject

---
+++ 16_provide和inject/src/components/GrandSon.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="grandSon">
+        <h1>grandSon组件（孙），{{ name }}--{{ age }}</h1>
+    </div>
+</template>
+
+<script>
+import { toRefs, inject } from 'vue'
+
+export default {
+    name: "GrandSon",
+    setup () {
+        let grandSonPerson = inject('personInfo')
+        return { ...toRefs(grandSonPerson) }
+    }
+}
+</script>
+
+<style>
+.grandSon {
+    padding: 10px;
+    background-color: palegoldenrod;
+}
+</style>
