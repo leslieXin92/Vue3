8ae3c0174df854bba856efb474e70223a4ea46dd
leslieXin92 <leslieXin92@gmail.com>
2022-08-21 02:47:38 +0800

    feat: css-loader

--- 11_webpack/11.2_css/src/js/element.js
+++ 11_webpack/11.2_css/src/js/element.js
@@ -1,3 +1,8 @@
+import "../css/style.css"
+// webpack打包时，不认为css文件是一个模块，需要css-loader去处理一下
+
+// import "css-loader!../css/style.css" // xxx-loader! 意思是
+
 const h1El = document.createElement("h1")
 h1El.innerHTML = "hello world"
 h1El.className = "title"
--- 11_webpack/11.2_css/webpack.config.js
+++ 11_webpack/11.2_css/webpack.config.js
@@ -5,5 +5,13 @@ module.exports = {
 	output: {
 		path: path.resolve(__dirname, "./dist"), // 必须是绝对路径
 		filename: "main.js"
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/, // 正则
+				use: ["css-loader"]
+			}
+		]
 	}
 }
