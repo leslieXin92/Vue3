19601b555814eb7a17581a8b64b548f3252de43d
leslieXin92 <leslieXin92@gmail.com>
2022-08-21 02:49:58 +0800

    update: tips

--- 11_webpack/11.2_css/src/js/element.js
+++ 11_webpack/11.2_css/src/js/element.js
@@ -1,7 +1,8 @@
 import "../css/style.css"
 // webpack打包时，不认为css文件是一个模块，需要css-loader去处理一下
 
-// import "css-loader!../css/style.css" // xxx-loader! 意思是
+// import "css-loader!../css/style.css"
+// xxx-loader! 意思是这个css需要用这个loader加载，现在更多是在webpack.config.js中配置
 
 const h1El = document.createElement("h1")
 h1El.innerHTML = "hello world"
