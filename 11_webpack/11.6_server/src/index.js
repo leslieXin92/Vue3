184afa1c20f6123152a7024d79a42b699e847937
leslieXin92 <leslieXin92@gmail.com>
2022-08-27 20:48:48 +0800

    feat: wepack watch

---
+++ 11_webpack/11.6_server/src/index.js
@@ -0,0 +1,18 @@
+import { sum } from "./js/math"
+const { priceFormat } = require("./js/format")
+import "./js/element"
+// import { createApp } from "vue"
+import { createApp } from "vue/dist/vue.esm-bundler"
+
+console.log(sum(1, 2))
+console.log(priceFormat())
+
+const app = createApp({
+	template: `<h1>{{ msg }}</h1>`,
+	data() {
+		return {
+			msg: "hello world"
+		}
+	}
+})
+app.mount("#app")
---
+++ 11_webpack/11.6_server/src/js/format.js
@@ -0,0 +1,8 @@
+const priceFormat = function () {
+	return "$0.00"
+}
+
+// Common.js导出
+module.exports = {
+	priceFormat
+}
