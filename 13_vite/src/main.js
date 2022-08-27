14173c79a08b267ab00b862cc7a112641d71ede5
leslieXin92 <leslieXin92@gmail.com>
2022-08-28 04:26:27 +0800

    feat: vite css

--- 13_vite/src/main.js
+++ 13_vite/src/main.js
@@ -1,7 +1,14 @@
 import _ from "lodash-es"
 import { sum } from "./js/math"
+import "./css/style.css"
+import "./css/title.less"
 
 console.log("hello world")
 console.log(sum(1, 2))
 
 console.log(_.join(["aaa", "bbb"], "-"))
+
+const titleEl = document.createElement("div")
+titleEl.className = "title"
+titleEl.innerHTML = "hello vite"
+document.body.appendChild(titleEl)
