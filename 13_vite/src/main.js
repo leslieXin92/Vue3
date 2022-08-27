d94d8e96b7db9e71dece406819ccbf7b5cc22b59
leslieXin92 <leslieXin92@gmail.com>
2022-08-28 04:35:11 +0800

    feat: vite ts

--- 13_vite/src/main.js
+++ 13_vite/src/main.js
@@ -1,10 +1,12 @@
 import _ from "lodash-es"
 import { sum } from "./js/math"
+import mul from "./ts/mul"
 import "./css/style.css"
 import "./css/title.less"
 
 console.log("hello world")
 console.log(sum(1, 2))
+console.log(mul(2, 3))
 
 console.log(_.join(["aaa", "bbb"], "-"))
 
