8c638c25dbb6e015b528e9f99c249539e52f961e
leslieXin92 <leslieXin92@gmail.com>
2022-08-28 01:05:33 +0800

    feat: webpack devServer配置

--- 11_webpack/11.7_server/webpack.config.js
+++ 11_webpack/11.7_server/webpack.config.js
@@ -14,7 +14,11 @@ module.exports = {
 	target: "web", // 为哪个环境打包
 	devServer: {
 		// contentBase: "./dist",
-		hot: true // 模块热替换（按模块更新而非整个页面）
+		hot: true, // 模块热替换（按模块更新而非整个页面）
+		host: "0.0.0.0",
+		port: 7777,
+		open: true,
+		compress: true // 静态文件压缩gzip
 	},
 	module: {
 		rules: [
