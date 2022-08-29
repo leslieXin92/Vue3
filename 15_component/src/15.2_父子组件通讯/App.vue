69d1f9f685b2841dedc12f58abfc1549cb74ff62
leslieXin92 <leslieXin92@gmail.com>
2022-08-30 01:20:05 +0800

    feat: props

---
+++ 15_component/src/15.2_父子组件通讯/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <show-msg :msg="msg" />
+</template>
+
+<script>
+import ShowMsg from "./ShowMsg.vue"
+
+export default {
+  components: { ShowMsg },
+  data() {
+    return {
+      msg: 'hello world'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
--- 15_component/src/main.js
+++ 15_component/src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from "vue"
-import App from "./15.1_组件的拆分和嵌套/App.vue"
+
+// import App from "./15.1_组件的拆分和嵌套/App.vue"
+import App from "./15.2_父子组件通讯/App.vue"
 
 createApp(App).mount("#app")
