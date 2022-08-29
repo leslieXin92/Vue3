47406262e3b41d936820c3557e9755b1e9b4bb1a
leslieXin92 <leslieXin92@gmail.com>
2022-08-30 01:45:43 +0800

    feat: emits

--- 15_component/src/15.2_父子组件通讯/App.vue
+++ 15_component/src/15.2_父子组件通讯/App.vue
@@ -1,15 +1,33 @@
 <template>
   <show-msg :msg="msg" />
+  <h1>{{  counter  }}</h1>
+  <counter @add="addOne" @sub="subOne" @addN="addN" @subN="subN" />
 </template>
 
 <script>
 import ShowMsg from "./ShowMsg.vue"
+import Counter from "./Counter.vue"
 
 export default {
-  components: { ShowMsg },
+  components: { ShowMsg, Counter },
   data() {
     return {
-      msg: 'hello world'
+      msg: "hello world",
+      counter: 10
+    }
+  },
+  methods: {
+    addOne() {
+      this.counter++
+    },
+    subOne() {
+      this.counter--
+    },
+    addN(n) {
+      this.counter += n
+    },
+    subN(n) {
+      this.counter -= n
     }
   }
 }
