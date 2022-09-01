a308b52174ac07d4150ddf073dfbab6ea1f2a4c6
leslieXin92 <leslieXin92@gmail.com>
2022-09-02 01:59:31 +0800

    feat: 动态组件基本使用

---
+++ 15_component/src/15.6_动态组件/App.vue
@@ -0,0 +1,39 @@
+<template>
+	<button
+		v-for="item in tabs"
+		:key="item"
+		@click="handleClick(item)"
+		:class="{ active: currentTab === item }"
+	>
+		{{ item }}
+	</button>
+
+	<component :is="currentTab" />
+</template>
+
+<script>
+import Home from '@/15.6_动态组件/Home.vue'
+import About from '@/15.6_动态组件/About.vue'
+import Category from '@/15.6_动态组件/Category.vue'
+
+export default {
+	components: { Home, About, Category },
+	data() {
+		return {
+			tabs: ['home', 'about', 'category'],
+			currentTab: 'home'
+		}
+	},
+	methods: {
+		handleClick(item) {
+			this.currentTab = item
+		}
+	}
+}
+</script>
+
+<style scoped>
+.active {
+	color: red;
+}
+</style>
--- 15_component/src/main.js
+++ 15_component/src/main.js
@@ -4,6 +4,7 @@ import { createApp } from 'vue'
 // import App from './15.2_父子组件通讯/App.vue'
 // import App from './15.3_Provide和Inject/App.vue'
 // import App from './15.4_事件总线/App.vue'
-import App from './15.5_插槽/App.vue'
+// import App from './15.5_插槽/App.vue'
+import App from './15.6_动态组件/App.vue'
 
 createApp(App).mount('#app')
