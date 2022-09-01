f6eccdeb505183ec0acc56cbd91980efd1f643a5
leslieXin92 <leslieXin92@gmail.com>
2022-09-01 23:48:34 +0800

    feat: 插槽基本使用

---
+++ 15_component/src/15.5_插槽/App.vue
@@ -0,0 +1,15 @@
+<template>
+	<my-slot>
+		<h1>slot</h1>
+	</my-slot>
+</template>
+
+<script>
+import MySlot from '@/15.5_插槽/MySlot.vue'
+
+export default {
+	components: { MySlot }
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ 15_component/src/15.5_插槽/MySlot.vue
@@ -0,0 +1,13 @@
+<template>
+	<h1>组件开始</h1>
+
+	<slot> </slot>
+
+	<h1>组件结束</h1>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped></style>
