d7ff27544883223fcd38f7108cd1a13090ae146b
leslieXin92 <leslieXin92@gmail.com>
2022-09-02 00:54:18 +0800

    feat: 作用域插槽基本使用

--- 15_component/src/15.5_插槽/App.vue
+++ 15_component/src/15.5_插槽/App.vue
@@ -1,29 +1,49 @@
 <template>
+	<!-- 匿名插槽 -->
 	<my-slot>
 		<h1>匿名插槽内容</h1>
 	</my-slot>
 
 	<hr />
 
+	<!-- 具名插槽 -->
 	<name-slot>
 		<template v-slot:name>
-			<h1>leslie</h1>
+			<h1>{{ info.name }}</h1>
 		</template>
 		<template #age>
-			<h1>24</h1>
+			<h1>{{ info.age }}</h1>
 		</template>
 		<template #default>
 			<h1>cabbage</h1>
 		</template>
 	</name-slot>
+
+	<hr />
+
+	<!-- 作用域插槽 -->
+	<scoped-slot :info="info">
+		<template v-slot="slotProps">
+			<h1>{{ slotProps.itemKey }} : {{ slotProps.itemValue }}</h1>
+		</template>
+	</scoped-slot>
 </template>
 
 <script>
 import MySlot from '@/15.5_插槽/MySlot.vue'
 import NameSlot from '@/15.5_插槽/NameSlot.vue'
+import ScopedSlot from '@/15.5_插槽/ScopedSlot.vue'
 
 export default {
-	components: { MySlot, NameSlot }
+	components: { MySlot, NameSlot, ScopedSlot },
+	data() {
+		return {
+			info: {
+				name: 'leslie',
+				age: 24
+			}
+		}
+	}
 }
 </script>
 
---
+++ 15_component/src/15.5_插槽/ScopedSlot.vue
@@ -0,0 +1,22 @@
+<template>
+	<h1>作用域插槽开始</h1>
+
+	<template v-for="(value, key) in info" :key="key">
+		<slot :itemKey="key" :itemValue="value">{{ key }} : {{ value }}</slot>
+	</template>
+
+	<h1>作用域插槽结束</h1>
+</template>
+
+<script>
+export default {
+	props: {
+		info: {
+			type: Object,
+			default: () => {}
+		}
+	}
+}
+</script>
+
+<style lang="scss" scoped></style>
