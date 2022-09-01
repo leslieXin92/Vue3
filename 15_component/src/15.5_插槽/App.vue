21765d5597aacef113c7ea83fce418faccc40c99
leslieXin92 <leslieXin92@gmail.com>
2022-09-02 00:23:18 +0800

    feat: 具名插槽基本使用

--- 15_component/src/15.5_插槽/App.vue
+++ 15_component/src/15.5_插槽/App.vue
@@ -1,14 +1,29 @@
 <template>
 	<my-slot>
-		<h1>slot</h1>
+		<h1>匿名插槽内容</h1>
 	</my-slot>
+
+	<hr />
+
+	<name-slot>
+		<template v-slot:name>
+			<h1>leslie</h1>
+		</template>
+		<template #age>
+			<h1>24</h1>
+		</template>
+		<template #default>
+			<h1>cabbage</h1>
+		</template>
+	</name-slot>
 </template>
 
 <script>
 import MySlot from '@/15.5_插槽/MySlot.vue'
+import NameSlot from '@/15.5_插槽/NameSlot.vue'
 
 export default {
-	components: { MySlot }
+	components: { MySlot, NameSlot }
 }
 </script>
 
--- 15_component/src/15.5_插槽/NameSlot.vue
+++ 15_component/src/15.5_插槽/NameSlot.vue
@@ -1,9 +1,11 @@
 <template>
-	<h1>匿名插槽开始</h1>
+	<h1>具名插槽开始</h1>
 
+	<slot name="name"> </slot>
+	<slot name="age"> </slot>
 	<slot> </slot>
 
-	<h1>匿名插槽结束</h1>
+	<h1>具名插槽结束</h1>
 </template>
 
 <script>
