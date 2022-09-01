b73750d4fea24a1af23e8e55b48a21e753cf2680
leslieXin92 <leslieXin92@gmail.com>
2022-09-01 22:29:12 +0800

    feat: Provide & Inject响应式传递

--- 15_component/src/15.3_/App.vue
+++ 15_component/src/15.3_/App.vue
@@ -1,16 +1,26 @@
 <template>
 	<home />
+	<button @click="age++">age++</button>
 </template>
 
 <script>
 import Home from '@/15.3_/Home.vue'
+import { computed } from 'vue'
 
 export default {
 	name: 'App',
 	components: { Home },
-	provide: {
-		name: 'leslie',
-		age: 23
+	provide() {
+		return {
+			name: this.name, // 非响应式
+			age: computed(() => this.age) // 响应式
+		}
+	},
+	data() {
+		return {
+			name: 'leslie',
+			age: 23
+		}
 	}
 }
 </script>
