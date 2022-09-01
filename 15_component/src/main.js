621eceb8c2a63ef4bc4bd4f85e7f565a0580e663
leslieXin92 <leslieXin92@gmail.com>
2022-09-01 22:40:41 +0800

    update(15.3): file name

--- 15_component/src/main.js
+++ 15_component/src/main.js
@@ -2,6 +2,6 @@ import { createApp } from 'vue'
 
 // import App from './15.1_组件的拆分和嵌套/App.vue'
 // import App from './15.2_父子组件通讯/App.vue'
-import App from './15.3_/App.vue'
+import App from './15.3_Provide和Inject/App.vue'
 
 createApp(App).mount('#app')
