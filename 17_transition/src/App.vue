fb6829a9438c511d40f43fee8a87b923e67dc566
leslieXin92 <leslieXin92@gmail.com>
2022-09-03 00:04:38 +0800

    feat: transition基本使用

---
+++ 17_transition/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+	<button @click="isShow = !isShow">show / hide</button>
+
+	<transition name="transition">
+		<h1 v-if="isShow">hello world</h1>
+	</transition>
+</template>
+
+<script>
+export default {
+	name: 'App',
+	data() {
+		return {
+			isShow: true
+		}
+	}
+}
+</script>
+
+<style scoped>
+.transition-enter-from,
+.transition-leave-to {
+	opacity: 0;
+}
+
+.transition-enter-to,
+.transition-leave-from {
+	opacity: 1;
+}
+
+.transition-enter-active,
+.transition-leave-active {
+	transition: opacity 0.3s ease;
+}
+</style>
