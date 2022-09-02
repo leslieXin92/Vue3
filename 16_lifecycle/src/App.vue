4c39cc32cc9362e632ce3b9601d9fdb6f143ba1f
leslieXin92 <leslieXin92@gmail.com>
2022-09-02 20:52:08 +0800

    feat: lifecycle

---
+++ 16_lifecycle/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+	<home v-if="showHome" />
+	<button @click="showHome = !showHome">unmount Home</button>
+</template>
+
+<script>
+import Home from './Home.vue'
+
+export default {
+	name: 'App',
+	components: { Home },
+	data() {
+		return {
+			showHome: true
+		}
+	}
+}
+</script>
+
+<style scoped></style>
---
+++ 16_lifecycle/src/Home.vue
@@ -0,0 +1,43 @@
+<template>
+	<h1>name: {{ name }}</h1>
+	<h1>age: {{ age }}</h1>
+	<button @click="age++">age++</button>
+</template>
+
+<script>
+export default {
+	name: 'Home',
+	beforeCreate() {
+		console.log('---- beforeCreate')
+	},
+	created() {
+		console.log('---- created')
+	},
+	beforeMount() {
+		console.log('---- beforeMount')
+	},
+	mounted() {
+		console.log('---- mounted')
+	},
+	beforeUpdate() {
+		console.log('---- beforeUpdate')
+	},
+	updated() {
+		console.log('---- updated')
+	},
+	beforeUnmount() {
+		console.log('---- beforeUnmount')
+	},
+	unmounted() {
+		console.log('---- unmounted')
+	},
+	data() {
+		return {
+			name: 'leslie',
+			age: 24
+		}
+	}
+}
+</script>
+
+<style scoped></style>
