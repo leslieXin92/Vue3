5f0380f9ab9679f57e6f55580093c4ca2194fbb4
leslieXin92 <leslieXin92@gmail.com>
2022-09-04 02:57:00 +0800

    feat: setup watch options

--- 18_composition-api/src/18.2_setup/Watch.vue
+++ 18_composition-api/src/18.2_setup/Watch.vue
@@ -1,10 +1,12 @@
 <template>
 	<h1>boyName: {{ boyInfo.name }}</h1>
 	<h1>boyAge: {{ boyInfo.age }}</h1>
+	<h1>boyLover: {{ boyInfo.lover }}</h1>
 	<h1>girlName: {{ girlName }}</h1>
 	<h1>girlAge: {{ girlAge }}</h1>
 	<button @click="changeName">changeName</button>
 	<button @click="incrementAge">incrementAge</button>
+	<button @click="changeBoyLoverAge">changeBoyLoverAge</button>
 </template>
 
 <script>
@@ -15,7 +17,11 @@ export default {
 	setup() {
 		const boyInfo = reactive({
 			name: 'leslie',
-			age: 24
+			age: 24,
+			lover: {
+				name: 'cabbage',
+				age: 23
+			}
 		})
 		const girlName = ref('cabbage')
 		const girlAge = ref(23)
@@ -28,22 +34,29 @@ export default {
 			boyInfo.age++
 			girlAge.value++
 		}
+		const changeBoyLoverAge = () => {
+			boyInfo.lover.age++
+		}
 
 		// 1、监听reactive，返回的newValue和oldValue都是reactive对象
-		// watch(boyInfo, (newValue, oldValue) => {
-		// 	console.log('newInfo', newValue)
-		// 	console.log('oldInfo', oldValue)
-		// })
+		watch(boyInfo, (newValue, oldValue) => {
+			console.log('newInfo', newValue)
+			console.log('oldInfo', oldValue)
+		})
 
 		// 2、监听reactive的各个/单个属性，返回的newValue和oldValue都是普通对象
-		// watch(
-		// 	// () => boyInfo.age,
-		// 	() => ({ ...boyInfo }),
-		// 	(newValue, oldValue) => {
-		// 		console.log('newValue', newValue)
-		// 		console.log('oldValue', oldValue)
-		// 	}
-		// )
+		watch(
+			// () => ({ ...boyInfo }),
+			() => boyInfo.lover,
+			(newValue, oldValue) => {
+				console.log('newValue', newValue)
+				console.log('oldValue', oldValue)
+			},
+			{
+				deep: true,
+				immediate: true
+			}
+		)
 
 		// 3、监听多个数据源
 		watch([girlName, girlAge], ([newName, newAge], [oldName, oldAge]) => {
@@ -56,7 +69,8 @@ export default {
 			girlName,
 			girlAge,
 			changeName,
-			incrementAge
+			incrementAge,
+			changeBoyLoverAge
 		}
 	}
 }
