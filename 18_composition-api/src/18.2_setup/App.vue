a8c21111385ec30d29b5ba4d847f93cd8cdefaa0
leslieXin92 <leslieXin92@gmail.com>
2022-09-04 02:24:41 +0800

    feat: setup watch基本使用

--- 18_composition-api/src/18.2_setup/App.vue
+++ 18_composition-api/src/18.2_setup/App.vue
@@ -10,16 +10,19 @@
 	<watch-effect />
 
 	<hr />
+
+	<watch />
 </template>
 
 <script>
 import Ref_Reactive from './Ref_Reactive.vue'
 import Computed from './Computed.vue'
 import WatchEffect from './WatchEffect.vue'
+import Watch from './Watch.vue'
 
 export default {
 	name: 'App',
-	components: { Ref_Reactive, Computed, WatchEffect }
+	components: { Ref_Reactive, Computed, WatchEffect, Watch }
 }
 </script>
 
---
+++ 18_composition-api/src/18.2_setup/Watch.vue
@@ -0,0 +1,49 @@
+<template>
+	<h1>name: {{ info.name }}</h1>
+	<h1>age: {{ info.age }}</h1>
+	<button @click="changeName">changeName</button>
+	<button @click="incrementAge">incrementAge</button>
+</template>
+
+<script>
+import { reactive, watch } from 'vue'
+
+export default {
+	name: 'Watch',
+	setup() {
+		const info = reactive({
+			name: 'leslie',
+			age: 24
+		})
+
+		const changeName = () => {}
+		const incrementAge = () => {
+			info.age++
+		}
+
+		// 1、监听reactive，返回的newValue和oldValue都是reactive对象
+		// watch(info, (newValue, oldValue) => {
+		// 	console.log('newInfo', newValue)
+		// 	console.log('oldInfo', oldValue)
+		// })
+
+		// 2、监听reactive各个/单个属性，返回的newValue和oldValue都是普通对象
+		watch(
+			// () => info.age,
+			() => ({ ...info }),
+			(newValue, oldValue) => {
+				console.log('newValue', newValue)
+				console.log('oldValue', oldValue)
+			}
+		)
+
+		return {
+			info,
+			changeName,
+			incrementAge
+		}
+	}
+}
+</script>
+
+<style scoped></style>
