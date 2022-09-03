df5c6e330ff36364821dcd8b93d8e3306c5d9d6c
leslieXin92 <leslieXin92@gmail.com>
2022-09-03 22:34:41 +0800

    feat: setup watchEffect

--- 18_composition-api/src/18.2_setup/App.vue
+++ 18_composition-api/src/18.2_setup/App.vue
@@ -4,15 +4,20 @@
 	<hr />
 
 	<computed />
+
+	<hr />
+
+	<watch />
 </template>
 
 <script>
 import Ref_Reactive from './Ref_Reactive.vue'
 import Computed from './Computed.vue'
+import Watch from './Watch.vue'
 
 export default {
 	name: 'App',
-	components: { Ref_Reactive, Computed }
+	components: { Ref_Reactive, Computed, Watch }
 }
 </script>
 
---
+++ 18_composition-api/src/18.2_setup/Watch.vue
@@ -0,0 +1,35 @@
+<template>
+	<h1>name: {{ name }}</h1>
+	<h1>age: {{ age }}</h1>
+	<button @click="changeName">changeName</button>
+	<button @click="incrementAge">incrementAge</button>
+</template>
+
+<script>
+import { ref, watchEffect, watch } from 'vue'
+
+export default {
+	name: 'Watch',
+	setup() {
+		const name = ref('leslie')
+		const age = ref(24)
+		const changeName = () => (name.value += '!')
+		const incrementAge = () => age.value++
+
+		// watchEffect：自动收集依赖 并 立即调用
+		watchEffect(() => {
+			console.log('name', name.value)
+			console.log('age', age.value)
+		})
+
+		return {
+			name,
+			age,
+			changeName,
+			incrementAge
+		}
+	}
+}
+</script>
+
+<style scoped></style>
