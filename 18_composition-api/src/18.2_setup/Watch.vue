1588f4224141d3df2a1ad991b2fd74590b637ff3
leslieXin92 <leslieXin92@gmail.com>
2022-09-04 01:40:41 +0800

    feat: setup watchEffect触发时机

--- 18_composition-api/src/18.2_setup/Watch.vue
+++ 18_composition-api/src/18.2_setup/Watch.vue
@@ -1,6 +1,7 @@
 <template>
 	<h1>name: {{ name }}</h1>
 	<h1>age: {{ age }}</h1>
+	<h1 ref="yahoo">yahoo</h1>
 	<button @click="changeName">changeName</button>
 	<button @click="incrementAge">incrementAge</button>
 </template>
@@ -23,27 +24,44 @@ export default {
 
 		// watchEffect：自动收集依赖 并 立即调用
 		// onInvalidate 为 watchEffect清除副作用
-		watchEffect(onInvalidate => {
-			const timer = setTimeout(() => {
-				console.log('请求成功')
-			}, 3000)
-			// 根据监听对象(name)发请求
-			onInvalidate(() => {
-				// 清除额外的副作用
-				clearTimeout(timer)
-				console.log('上次请求取消，开始下次请求')
-			})
-			console.log('name变了', name.value)
-		})
+		watchEffect(
+			onInvalidate => {
+				const timer = setTimeout(() => {
+					console.log('请求成功')
+				}, 2000)
+				// 根据监听对象(name)发请求
+				onInvalidate(() => {
+					// 清除额外的副作用
+					clearTimeout(timer)
+					console.log('上次请求取消，开始下次请求')
+				})
+				console.log('name变了', name.value)
+			},
+			{
+				flush: 'post'
+			}
+		)
 
 		// watchEffect停止侦听
 		const stopAgeWatchEffect = watchEffect(() => {
 			console.log('age变了', age.value)
 		})
 
+		// watchEffect触发时机
+		const yahoo = ref(null)
+		watchEffect(
+			() => {
+				console.log('yahoo', yahoo.value)
+			},
+			{
+				flush: 'post' // dom构建后再监听
+			}
+		)
+
 		return {
 			name,
 			age,
+			yahoo,
 			changeName,
 			incrementAge
 		}
