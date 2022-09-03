3311a3e717eadf5f205c579c191fceae58df6f23
leslieXin92 <leslieXin92@gmail.com>
2022-09-04 02:40:14 +0800

    feat: setup watch监听多个数据源

--- 18_composition-api/src/18.2_setup/Watch.vue
+++ 18_composition-api/src/18.2_setup/Watch.vue
@@ -1,44 +1,60 @@
 <template>
-	<h1>name: {{ info.name }}</h1>
-	<h1>age: {{ info.age }}</h1>
+	<h1>boyName: {{ boyInfo.name }}</h1>
+	<h1>boyAge: {{ boyInfo.age }}</h1>
+	<h1>girlName: {{ girlName }}</h1>
+	<h1>girlAge: {{ girlAge }}</h1>
 	<button @click="changeName">changeName</button>
 	<button @click="incrementAge">incrementAge</button>
 </template>
 
 <script>
-import { reactive, watch } from 'vue'
+import { ref, reactive, watch } from 'vue'
 
 export default {
 	name: 'Watch',
 	setup() {
-		const info = reactive({
+		const boyInfo = reactive({
 			name: 'leslie',
 			age: 24
 		})
+		const girlName = ref('cabbage')
+		const girlAge = ref(23)
 
-		const changeName = () => {}
+		const changeName = () => {
+			boyInfo.name += '!'
+			girlName.value += '!'
+		}
 		const incrementAge = () => {
-			info.age++
+			boyInfo.age++
+			girlAge.value++
 		}
 
 		// 1、监听reactive，返回的newValue和oldValue都是reactive对象
-		// watch(info, (newValue, oldValue) => {
+		// watch(boyInfo, (newValue, oldValue) => {
 		// 	console.log('newInfo', newValue)
 		// 	console.log('oldInfo', oldValue)
 		// })
 
-		// 2、监听reactive各个/单个属性，返回的newValue和oldValue都是普通对象
-		watch(
-			// () => info.age,
-			() => ({ ...info }),
-			(newValue, oldValue) => {
-				console.log('newValue', newValue)
-				console.log('oldValue', oldValue)
-			}
-		)
+		// 2、监听reactive的各个/单个属性，返回的newValue和oldValue都是普通对象
+		// watch(
+		// 	// () => boyInfo.age,
+		// 	() => ({ ...boyInfo }),
+		// 	(newValue, oldValue) => {
+		// 		console.log('newValue', newValue)
+		// 		console.log('oldValue', oldValue)
+		// 	}
+		// )
+
+		// 3、监听多个数据源
+		watch([girlName, girlAge], ([newName, newAge], [oldName, oldAge]) => {
+			console.log(`${oldName} => ${newName}`)
+			console.log(`${oldAge} => ${newAge}`)
+		})
 
 		return {
-			info,
+			boyInfo,
+			girlName,
+			girlAge,
 			changeName,
 			incrementAge
 		}
