2fa14e2d49b19ffe01dab7947f11dd05faa9f352
leslieXin92 <leslieXin92@gmail.com>
2022-09-11 05:59:18 +0800

    feat: Teleport

---
+++ 21_Telepor/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+	<teleport to="#yahoo">
+		<h1>counter: {{ counter }}</h1>
+		<button @click="counter++">counter++</button>
+	</teleport>
+</template>
+
+<script>
+import { ref } from 'vue'
+export default {
+	setup() {
+		const counter = ref(0)
+		return { counter }
+	}
+}
+</script>
