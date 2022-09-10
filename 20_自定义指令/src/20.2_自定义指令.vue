af51cc05d34491cf05dd648504800ace21228555
leslieXin92 <leslieXin92@gmail.com>
2022-09-11 05:17:46 +0800

    feat: 自定义指令生命周期

--- 20_自定义指令/src/20.2_自定义指令.vue
+++ 20_自定义指令/src/20.2_自定义指令.vue
@@ -1,19 +1,42 @@
 <template>
 	<input type="text" v-focus />
+	<h1>counter: {{ counter }}</h1>
+	<button @click="counter++" v-yahoo>counter++</button>
 </template>
 
 <script>
+import { ref } from 'vue'
+
 export default {
-	// // 局部自定义指令
-	// directives: {
-	// 	focus: {
-	// 		mounted(el, bindings, vnode, preVnode) {
-	// 			console.log('局部自定义指令 - focus - mounted')
-	// 			el.focus()
-	// 		}
-	// 	}
-	// }
+	// 局部自定义指令
+	directives: {
+		yahoo: {
+			created() {
+				console.log('局部自定义指令 - yahoo - created')
+			},
+			beforeMount() {
+				console.log('局部自定义指令 - yahoo - beforeMount')
+			},
+			mounted(el, bindings, vnode, preVnode) {
+				console.log('局部自定义指令 - yahoo - mounted')
+			},
+			beforeUpdate() {
+				console.log('局部自定义指令 - yahoo - beforeUpdate')
+			},
+			updated() {
+				console.log('局部自定义指令 - yahoo - updated')
+			},
+			beforeUnmount() {
+				console.log('局部自定义指令 - yahoo - beforeUnmount')
+			},
+			unmounted() {
+				console.log('局部自定义指令 - yahoo - unmounted')
+			}
+		}
+	},
+	setup() {
+		const counter = ref(0)
+		return { counter }
+	}
 }
 </script>
-
-<style lang="scss" scoped></style>
