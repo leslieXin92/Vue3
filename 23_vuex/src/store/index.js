29d192f47f42b49c5d9421276d64157f41291245
leslieXin92 <leslieXin92@gmail.com>
2022-09-12 09:06:53 +0800

    feat: getters基本用法

--- 23_vuex/src/store/index.js
+++ 23_vuex/src/store/index.js
@@ -5,7 +5,14 @@ const store = createStore({
 		return {
 			name: 'leslie',
 			age: 24,
-			counter: 0
+			counter: 0,
+			books: [
+				{ name: 'Vue', price: 100, count: 30 },
+				{ name: 'node', price: 200, count: 30 },
+				{ name: 'webpack', price: 150, count: 20 },
+				{ name: 'vite', price: 150, count: 15 }
+			],
+			discount: 0.75
 		}
 	},
 	mutations: {
@@ -14,6 +21,14 @@ const store = createStore({
 		},
 		decrement(state) {
 			state.counter--
+		}
+	},
+	getters: {
+		curDiscount(state) {
+			return state.discount * 0.9
+		},
+		totalPrice(state, getters) {
+			return state.books.map(item => item.price * item.count).reduce((pre, cur) => pre + cur, 0) * getters.curDiscount
 		}
 	}
 })
